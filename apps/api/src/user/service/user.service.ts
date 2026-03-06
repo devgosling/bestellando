e2788ae1eb4f2b440e8fae7b41d16f18a8114f3d
devgosling <lastweekend1592@gmail.com>
2026-03-06 12:49:43 +0100

    feat: update getUserType method to return UserType or null

--- apps/api/src/user/service/user.service.ts
+++ apps/api/src/user/service/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { ActorContextService } from "src/auth/service/actor-context.service";
 import { AppwriteService } from "src/auth/service/appwrite.service";
+import { UserType } from "../interface/user.interface";
 
 @Injectable()
 export class UserService {
@@ -9,12 +10,16 @@ export class UserService {
     private readonly appwriteService: AppwriteService,
   ) {}
 
-  public async getUserType() {
+  public async getUserType() : Promise<UserType | null> {
     const context = this.actorContextService.get();
 
     const appwriteUser = context?.user?.appwrite;
     if (!appwriteUser) {
       return null;
     }
+
+    // TODO: Check user team memberships to determine user type
+    // For now, we will just return "CUSTOMER" for all users
+    return "CUSTOMER";
   }
 }
